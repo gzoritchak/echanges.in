a69db2c0cae05af1f2a7520728ba2cc28907c319
Gaetan Zoritchak <g.zoritchak@gmail.com>
2013-01-06 00:14:00 +0100

    Mise à jour automatique de la base au démarrage de l'application.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -91,3 +91,4 @@ log4j = {
 }
 
 grails.plugin.databasemigration.updateOnStart=true //mise à jour automatique de la base au démarrage.
+grails.plugin.databasemigration.updateOnStartFileNames = ['changelog.groovy']
