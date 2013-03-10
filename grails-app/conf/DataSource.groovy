32bc43e587d4a719ae4f1ba2263a823c05ba80c3
Gaetan Zoritchak <g.zoritchak@gmail.com>
2013-03-10 22:08:08 +0100

    Paramétrage Datasource pour utilisation sur Heroku.

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,12 +1,3 @@
-dataSource {
-    driverClassName = "org.postgresql.Driver"
-    username = "echangesin"
-    password = "echangesin"
-    url = "jdbc:postgresql://localhost:5432/echangesin"
-    dialect = org.hibernate.dialect.PostgreSQLDialect
-    hibernate.default_schema = "public"
-    pooled = true
-}
 hibernate {
     cache.use_second_level_cache = true
     cache.use_query_cache = false
@@ -16,14 +7,25 @@ hibernate {
 environments {
     production {
         dataSource {
-//            dbCreate = "validate"
+            dbCreate = "update"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            uri = new URI(System.env.DATABASE_URL ?: "postgres://test:test@localhost/test")
+            url = "jdbc:postgresql://" + uri.host + uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
         }
     }
     development {
         dataSource {
-//            dbCreate = "validate"
-//            dbCreate = "update"
-            }
+            driverClassName = "org.postgresql.Driver"
+            username = "echangesin"
+            password = "echangesin"
+            url = "jdbc:postgresql://localhost:5432/echangesin"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+            hibernate.default_schema = "public"
+            pooled = true
+        }
     }
     test {
         dataSource {
