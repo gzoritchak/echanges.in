4b714191c3113c1885ebee98c55ec153c2c5c2e1
Gaetan Zoritchak <g.zoritchak@gmail.com>
2013-05-02 00:34:26 +0200

    Log info.

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -94,6 +94,7 @@ log4j = {
         console name:'stdout', layout:pattern(conversionPattern: '%c{2} %m%n')
     //}
 
+    info   'org.codehaus.groovy.grails.commons'
     error  'org.codehaus.groovy.grails.web.servlet',        // controllers
            'org.codehaus.groovy.grails.web.pages',          // GSP
            'org.codehaus.groovy.grails.web.sitemesh',       // layouts
